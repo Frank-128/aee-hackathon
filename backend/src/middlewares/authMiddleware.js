4e34a4754e19f4e94b37b375a03e489ccfb84193
Frank Ndagula <107749616+Frank-128@users.noreply.github.com>
2026-01-31 06:38:21 +0530

    Refactor authorize middleware to check user roles

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -31,22 +31,17 @@ const protect = async (req, res, next) => {
     }
 };
 
-// const authorize = (...roles) => {
-//     return (req, res, next) => {
-//         if (!roles.includes(req.user.role)) {
-//             return res.status(403).json({
-//                 success: false,
-//                 message: `User role ${req.user.role} is not authorized to access this route`,
-//             });
-//         }
-//         next();
-//     };
-// };
-
 const authorize = (...roles) => {
     return (req, res, next) => {
-        next(); // allow everything
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: `User role ${req.user.role} is not authorized to access this route`,
+            });
+        }
+        next();
     };
 };
+
 
 module.exports = { protect, authorize };
